88b929ebd6cdba68f3570bf81b4f0e5c1c9620aa
akhil <akhil.vkl@gmail.com>
2021-10-31 12:19:36 -0400

    cpp data frame for nanoflann

--- twinning_cpp/src/twinning.cpp
+++ twinning_cpp/src/twinning.cpp
@@ -1,21 +1,58 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include <pybind11/stl.h>
 #include <nanoflann.hpp>
 #include <iostream>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-std::vector<std::size_t> twin_cpp(pybind11::array_t<double> data) 
-{
-    auto data_access = data.unchecked<2>();
-    std::cout << data_access(0, 3) << std::endl;
+namespace py = pybind11;
 
-    return {1, 2, 3};
+class DF
+{
+private:
+    std::shared_ptr<py::detail::unchecked_reference<double, 2>> data_access_;
+
+public:
+    DF(py::array_t<double> data)
+    {
+        data_access_ = std::make_shared<py::detail::unchecked_reference<double, 2>>(data.unchecked<2>());
+    }
+
+    inline std::size_t kdtree_get_point_count() const
+    {
+        return data_access_->shape(0);
+    }
+
+    inline double kdtree_get_pt(const std::size_t idx, const std::size_t dim) const 
+    {
+        return (*data_access_)(idx, dim);
+    }
+
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX&) const 
+    { 
+        return false; 
+    }
+};
+
+
+typedef nanoflann::KDTreeSingleIndexDynamicAdaptor<nanoflann::L2_Adaptor<double, DF>, DF, -1, std::size_t> kdTree;
+
+
+py::array_t<std::size_t> twin_cpp(py::array_t<double> data) 
+{
+    py::detail::unchecked_reference<double, 2> data_access = data.unchecked<2>();
+    std::cout << data_access(0, 10) << std::endl;
+    std::cout << data_access.shape(0) << std::endl;
+
+    DF new_data(data);
+    std::cout << new_data.kdtree_get_point_count() << std::endl;
+
+    py::array_t<std::size_t> vec(5);
+    return vec;
 }
 
-namespace py = pybind11;
 
 PYBIND11_MODULE(twinning_cpp, m) {
     m.doc() = R"pbdoc(
--- twinning_functions.py
+++ twinning_functions.py
@@ -1,4 +1,4 @@
-import twinning_cpp
+from twinning_cpp import twin_cpp
 import numpy as np
 
 def twin():
@@ -18,9 +18,11 @@ def twin():
 	data = np.random.multivariate_normal(mean=mu, cov=sigma, size=N)
 
 	print(data[0, 3])
-	vec = twinning_cpp.twin_cpp(data)
+	print(data[2, 0])
+	vec = twin_cpp(data)
 	print(vec)
 	print(type(vec))
+	print(vec.dtype)
 
 
 
